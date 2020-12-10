cb6cc67d46827d8c0726d903cadc5e98a6915b67
aungzinphyo <aungzinphyo94@gmail.com>
2020-12-10 16:36:20 +0630

    Add Loop

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,5 +1,12 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Start Git");
+
+        int count = 0;
+
+        while (count < 10) {
+            System.out.print(count + " ");
+            count++;
+        }
     }
 }
